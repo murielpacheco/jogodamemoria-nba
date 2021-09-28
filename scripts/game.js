1a8483323306ab21d14af0aa69e862bf88f2194f
Muriel Damazio <murieldps@gmail.com>
2021-09-28 10:14:02 -0300

    project done

---
+++ scripts/game.js
@@ -0,0 +1,104 @@
+const game = {
+  lockMode: false,
+  firstCard: null,
+  secondCard: null,
+
+  teams: [
+    'bucks',
+    'celtics',
+    'knicks',
+    'lakers',
+    'nets', 
+    'raptors',
+    'sixers',
+    'spurs',
+    'suns',
+    'warriors',
+  ],
+  cards: null,
+
+  setCard(id) {
+    let card = this.cards.filter((card) => card.id === id)[0];
+    if (card.flipped || this.lockMode) {
+      return false;
+    }
+
+    if (!this.firstCard) {
+      this.firstCard = card;
+      this.firstCard.flipped = true;
+      return true;
+    } else {
+      this.secondCard = card;
+      this.secondCard.flipped = true;
+      this.lockMode = true;
+      return true;
+    }
+  },
+
+  checkMatch() {
+    if (!this.firstCard || !this.secondCard) {
+      return false;
+    }
+    return this.firstCard.icon === this.secondCard.icon;
+  },
+
+  clearCards() {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+  unflipCards() {
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+  },
+
+  checkGameOver() {
+    return this.cards.filter((card) => !card.flipped).length == 0;
+  },
+
+  createCardsFromTeams() {
+    this.cards = [];
+
+    this.teams.forEach((team) => {
+      this.cards.push(this.createPairFromTeam(team));
+    });
+    this.cards = this.cards.flatMap((pair) => pair);
+    this.shuffleCards();
+    return this.cards;
+  },
+
+  createPairFromTeam(team) {
+    return [
+      {
+        id: this.createIdWithTeam(team),
+        icon: team,
+        flipped: false,
+      },
+      {
+        id: this.createIdWithTeam(team),
+        icon: team,
+        flipped: false,
+      },
+    ];
+  },
+
+  createIdWithTeam(team) {
+    return team + parseInt(Math.random() * 1000);
+  },
+
+  shuffleCards(cards) {
+    let currentIndex = this.cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [this.cards[randomIndex], this.cards[currentIndex]] = [
+        this.cards[currentIndex],
+        this.cards[randomIndex],
+      ];
+    }
+  },
+};
